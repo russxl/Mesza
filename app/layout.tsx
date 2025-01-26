cb2e1b4e88419a6b4806e54ea1b661457c4b1d81
russxl <obatayrussell@gmail.com>
2025-01-26 17:47:18 +0800

    fixed unused variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navigation from "./components/navigation";
-import Footer from "./components/footer";
+
 
 
 const geistSans = Geist({
